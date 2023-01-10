a5d296d33c841b8d28d24980ecdd263fb5ee1359
JessicaIOrtega <jes.ortega7@gmail.com>
2023-01-10 10:46:29 -0600

    last commit for now

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,7 +4,13 @@ var viewScores = document.getElementById('highBtn');
 var timerDiv = document.getElementById('timer');
 var choicesArray = document.getElementById('choices');
 
+var qTitle = document.getElementById('titleQuestion');
+var choices = document.getElementById('choices')
+
 var score = 0;
+
+var index = 0;
+
 var questionsHolder = [
     {
         questionTitle: '1. How many times did Ross get divorced?',
@@ -27,22 +33,23 @@ var questionsHolder = [
         answer: 'Unagi'
     },
     {
-        questionTitle: 'Who introduced Phoebe and Mike?',
+        questionTitle: '5. Who introduced Phoebe and Mike?',
         choices: ['Joey', 'Rachel', 'Ross', 'Chandler'],
         answer: 'Joey'
     }
 ]
 
+
+
 var start = document.querySelector('.startBtn')
 start.addEventListener('click', beginQuiz)
 
 function beginQuiz() {
-    // timerDiv = setInterval(clockTick, 1000)
-
-    timerCounter = 60
     startTimer()
-    //renderQuestion()
-    valapregu()
+    start.style.display = 'none';
+    timerCounter = 60
+    renderQuestion()
+
 }
 
 function startTimer() {
@@ -51,73 +58,56 @@ function startTimer() {
         timerDiv.textContent = timerCounter
         if (timerCounter === 0) {
             clearInterval(timer)
-            lostGame()
+            endGame()
         }
     }, 1000)
 }
 function loseTime() {
     timerCounter -= 5;
 }
-function lostGame() {
+function endGame() {
     localStorage.setItem('finalHighscore', score)
-    return window.location.href = 'highscores.html'
+    alert('This is the end of your Friends Quiz. Go to Highscores')
+    window.location.href = 'highscores.html'
 }
 
 function renderQuestion() {
-    var currentQuestion = questionsHolder[0]
+    var currentQuestion = questionsHolder[index]
+    console.log(currentQuestion);
     var qTitle = document.getElementById('titleQuestion')
     qTitle.textContent = currentQuestion.questionTitle
-
-    qTitle.innerHTML = ''
 
     currentQuestion.choices.forEach(function (choice, i) {
         var choiceBtn = document.createElement('button')
         choiceBtn.setAttribute('class', 'choice')
         choiceBtn.setAttribute('value', choice)
 
-        // choiceBtn.onclick = questionPick
+        choiceBtn.textContent = questionsHolder[index].choices[i];
 
-        choiceBtn.onclick = respuesta(choiceBtn.value);
-        qTitle.appendChild(choiceBtn)
-
+        choiceBtn.onclick = checkAnswer;
+        qTitle.appendChild(choiceBtn);
 
     })
+
+
 }
 
+function checkAnswer(event) {
+    if (event.target.value !== questionsHolder[index].answer) {
+        var textAnswer = document.getElementById('textAnswer');
+        alert('Oh no! you got it wrong');
+        timerCounter = timerCounter - 5;
+        index++;
+    } else {
+        alert('Whoop, you got it right!')
+        score += 10;
+        index++;
+    }
 
+    if (index === questionsHolder.length) {
+        endGame();
+    } else {
+        renderQuestion();
+    }
 
-
-
-// function valapregu() {
-
-
-//     for (var i = 0; i < questionsHolder.length; i++) {
-//         var qTitle = document.getElementById('titleQuestion')
-
-//         qTitle.innerHTML = questionsHolder[i].questionTitle;
-
-//         for (var x = 0; x < questionsHolder[i].choices.length; x++) {
-//             var picheboton = document.createElement('button')
-//             picheboton.setAttribute('class', 'choice')
-//             picheboton.setAttribute('value', questionsHolder[i].choices[x])
-//             qTitle.appendChild(picheboton);
-
-//             picheboton.onclick = questionPick(picheboton.value, questionsHolder[i].answer);
-
-//         }
-
-//     }
-// }
-// function questionPick(c, a) {
-
-
-
-//     if (c === a) {
-//         score += 10;
-//         //add you got it right!
-//     } else {
-//         loseTime();
-//     }
-
-// }
-
+}
